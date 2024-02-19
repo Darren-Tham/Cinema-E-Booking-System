4a258fcce971d4d9799fce32f5b405b931bb26a7
Steven Tran <steven29160@gmail.com>
2024-02-19 13:26:35 -0500

    movie model

--- src/server/src/main/java/com/server/cinema/movie/movie.java
+++ src/server/src/main/java/com/server/cinema/movie/movie.java
@@ -1,3 +1,53 @@
+package com.server.cinema.movie;
+
 public class movie {
 
+  private String name;
+  private String trailerLink;
+  private String imageLink;
+  private String description;
+
+  public movie(
+    String name,
+    String trailerLink,
+    String imageLink,
+    String description
+  ) {
+    this.name = name;
+    this.trailerLink = trailerLink;
+    this.imageLink = imageLink;
+    this.description = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getTrailerLink() {
+    return trailerLink;
+  }
+
+  public String getImageLink() {
+    return imageLink;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setTrailerLink(String trailerLink) {
+    this.trailerLink = trailerLink;
+  }
+
+  public void setImageLink(String imageLink) {
+    this.imageLink = imageLink;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
