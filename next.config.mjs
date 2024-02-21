636aee5a0c57568c4a98f5f8915363ecb26e7ab6
Steven Tran <steven29160@gmail.com>
2024-02-21 18:10:40 -0500

    fix setup makefile, removed id because it will auto generate

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+  output: "standalone",
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
