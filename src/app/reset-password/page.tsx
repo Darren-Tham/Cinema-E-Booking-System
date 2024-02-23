629ef2f3245e9e4470762a62605308e3fc3566a6
Darren Tham <darrent9859@gmail.com>
2024-02-23 18:17:53 -0500

    Finish forgot password and confirmation

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -4,47 +4,42 @@ import HomeNavbar from "@/components/HomeNavbar"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 
-export default function Login() {
+export default function ResetPassword() {
   const router = useRouter()
   return (
     <div className="min-h-screen bg-black flex flex-col">
       <HomeNavbar />
       <div className="grow grid place-items-center">
         <div className="bg-dark-jade rounded-sm p-8 w-96 flex flex-col gap-3 items-center">
-          <h1 className="h1">Welcome Back!</h1>
+          <h1 className="h1">Reset Password</h1>
+          <p className="text-white text-center">
+            A verification code has been sent to your associated email address.
+            Enter the code below along with your new password.
+          </p>
           <div className="flex flex-col gap-1 w-full">
-            <label htmlFor="email" className="label">
-              Email *
+            <label htmlFor="verification-code" className="label">
+              Verification Code
             </label>
-            <input type="text" id="email" className="input" />
+            <input type="text" id="verification-code" className="input" />
           </div>
           <div className="flex flex-col gap-1 w-full">
             <label htmlFor="password" className="label">
-              Password *
+              Password
             </label>
             <input type="password" id="password" className="input" />
-            <Link
-              href="/forgot-password"
-              className="text-bright-jade font-semibold w-max hover:scale-105 transition-transform duration-300"
-            >
-              Forgot Password?
-            </Link>
           </div>
           <button
             className="action-button w-full"
-            onClick={() => router.push("/")}
+            onClick={() => router.push("/reset-password-confirmation")}
           >
-            Log In
+            Reset Password
           </button>
           <div className="flex justify-center">
-            <p className="p-redirection">Don&apos;t Have An Account?</p>
-            <Link href="/registration" className="link-redirection">
-              Register Here
+            <p className="p-redirection">Back To</p>
+            <Link href="/login" className="link-redirection">
+              Login
             </Link>
           </div>
-          <p className="text-white font-semibold text-sm self-start">
-            * Required Field
-          </p>
         </div>
       </div>
     </div>
