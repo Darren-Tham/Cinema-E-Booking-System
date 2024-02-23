441a0d157a903e65c42546dc62ca12be014eba0f
ragegoney2 <gagemroney@gmail.com>
2024-02-23 16:43:41 -0500

    Added order confirmation, updated underline

--- src/app/order-confirmation/page.tsx
+++ src/app/order-confirmation/page.tsx
@@ -1,7 +1,8 @@
 export default function OrderConfirmation() {
 
-    const h1Styles = "font-bold text-5xl text-white text-center underline"
+    const h1Styles = "font-bold text-5xl text-white text-center"
     const h1Styles2 = "text-1xl text-white text-center"
+    const h1Styles3 = "font-bold text-5xl text-white text-center underline"
     const buttonStyles =
     "text-white w-max font-bold px-4 py-2 rounded-md hover:scale-105 transition-transform duration-300 mt-2 bg-dark-jade  min-w-[200px] min-h-[50px] underline";
     const buttonStyles2 =
@@ -16,7 +17,7 @@ export default function OrderConfirmation() {
         
         <div className="flex justify-center items-center">
           <div className="bg-dark-jade p-16 rounded-lg flex flex-col gap-4 items-center mt-2">
-            <h1 className={h1Styles}>Order Details</h1>
+            <h1 className={h1Styles3}>Order Details</h1>
             <h1 className={h1Styles2}>Order Confirmation Number: 62P-452QR8</h1>
             <h1 className={h1Styles2}>Date: Thursday, Febuary 22, 2024</h1>
             <h1 className={h1Styles2}>Location: University 16 Cinemas</h1>
