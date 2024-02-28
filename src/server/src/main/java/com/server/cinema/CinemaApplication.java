0efc6770eeb36b57c577f980cac7b5a7bcf8bf86
Darren Tham <darrent9859@gmail.com>
2024-02-28 15:29:13 -0500

    Update home page

--- src/server/src/main/java/com/server/cinema/CinemaApplication.java
+++ src/server/src/main/java/com/server/cinema/CinemaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CinemaApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(CinemaApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApplication.class, args);
+    }
 }
