e1358e00603e8dd3f0ff0b1b8a5b45d8cdbfce27
Darren Tham <darrent9859@gmail.com>
2024-02-29 16:15:37 -0500

    Finish ticket summary page

--- src/app/seats/page.tsx
+++ src/app/seats/page.tsx
@@ -1,20 +1,14 @@
 import Image from "next/image"
 import HomeNavbar from "@/components/HomeNavbar"
 import Seat from "@/components/Seat"
+import CheckoutBanner from "@/components/CheckoutBanner"
+import Link from "next/link"
 
 export default function SeatsPage() {
-  const h2Styles = "text-white font-semibold text-lg"
   return (
     <div className="min-h-screen bg-black flex flex-col">
       <HomeNavbar />
-      <div className="bg-jade p-5">
-        <div className="flex justify-between">
-          <h2 className={h2Styles}>Movie: Encanto</h2>
-          <h2 className={h2Styles}>Time: 12:00 PM</h2>
-        </div>
-        <h2 className={h2Styles}>Location: University 16 Cinema</h2>
-        <h2 className={h2Styles}>Date: Thursday, February 22, 2024</h2>
-      </div>
+      <CheckoutBanner />
       <div className="px-20 py-10 flex flex-col items-center gap-10">
         <div className="bg-neutral-500 h-96 w-full" />
         <div className="grid grid-cols-8 grid-rows-4 w-max gap-3">
@@ -59,9 +53,12 @@ export default function SeatsPage() {
           <div />
           <div />
         </div>
-        <button className="bg-jade text-white font-semibold w-max py-3 px-20 text-xl rounded-md self-end scale-transition">
+        <Link
+          href="/gay"
+          className="bg-jade text-white font-semibold w-max py-3 px-20 text-xl rounded-md self-end scale-transition"
+        >
           Next
-        </button>
+        </Link>
       </div>
     </div>
   )
