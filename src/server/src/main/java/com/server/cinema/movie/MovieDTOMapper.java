1f90c7ad929ce17912ec560fd2096e8c34460eea
Darren Tham <darrent9859@gmail.com>
2024-03-01 01:01:07 -0500

    Can add movies to database

--- src/server/src/main/java/com/server/cinema/movie/MovieDTOMapper.java
+++ src/server/src/main/java/com/server/cinema/movie/MovieDTOMapper.java
@@ -7,14 +7,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class MovieDTOMapper implements Function<Movie, MovieDTO> {
 
-  @Override
-  public MovieDTO apply(Movie movie) {
-    return new MovieDTO(
-      movie.getId(),
-      movie.getName(),
-      movie.getTrailerLink(),
-      movie.getImageLink(),
-      movie.getDescription()
-    );
-  }
+    @Override
+    public MovieDTO apply(Movie movie) {
+        return new MovieDTO(
+                movie.getId(),
+                movie.getName(),
+                movie.getTrailerLink(),
+                movie.getImageLink(),
+                movie.getDescription(),
+                movie.getCategory());
+    }
 }
