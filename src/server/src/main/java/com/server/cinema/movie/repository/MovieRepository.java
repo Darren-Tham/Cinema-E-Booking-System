c9bab823383c6dec24745c49de02366e69a75037
Darren Tham <darrent9859@gmail.com>
2024-03-03 01:37:41 -0500

    Add a query to findByMovieNameLike

--- src/server/src/main/java/com/server/cinema/movie/repository/MovieRepository.java
+++ src/server/src/main/java/com/server/cinema/movie/repository/MovieRepository.java
@@ -2,10 +2,14 @@ package com.server.cinema.movie.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.server.cinema.movie.entity.Movie;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    @Query("SELECT m FROM movie m WHERE m.movie_name LIKE %?1%")
     List<Movie> findByMovieNameLike(final String movieName);
+
 }
