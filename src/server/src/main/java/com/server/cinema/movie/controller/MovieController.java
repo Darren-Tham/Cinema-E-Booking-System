029945c6c4cb801473974826496da294d9932b39
Darren Tham <darrent9859@gmail.com>
2024-03-03 02:01:51 -0500

    Update MovieController.java

--- src/server/src/main/java/com/server/cinema/movie/controller/MovieController.java
+++ src/server/src/main/java/com/server/cinema/movie/controller/MovieController.java
@@ -1,10 +1,11 @@
-package com.server.cinema.movie;
+package com.server.cinema.movie.controller;
 
-import com.server.cinema.movie.MovieAddRequest;
-import com.server.cinema.movie.MovieDTO;
-import com.server.cinema.movie.MovieService;
 import java.util.List;
-import org.springframework.http.HttpHeaders;
+
+import com.server.cinema.movie.entity.Movie;
+import com.server.cinema.movie.service.MovieService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,40 +14,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/movies")
-public class MovieController {
+@RequestMapping("api/movie")
+final class MovieController {
 
     private final MovieService movieService;
 
-    public MovieController(MovieService movieService) {
+    @Autowired
+    private MovieController(final MovieService movieService) {
         this.movieService = movieService;
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> addMovie(@RequestBody MovieAddRequest request) {
-        movieService.addMovie(request);
-        return ResponseEntity.ok().build();
+    ResponseEntity<String> addMovie(@RequestBody final Movie movie) {
+        movieService.addMovie(movie);
+        return new ResponseEntity<>("Movie has been successfully added.", HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
-    public MovieDTO getMovie(@PathVariable("id") Integer movieId) {
+    @GetMapping("/{movieId}")
+    Movie getMovie(@PathVariable final int movieId) {
         return movieService.getMovie(movieId);
     }
 
     @GetMapping
-    public List<MovieDTO> getMovies() {
+    List<Movie> getMovies() {
         return movieService.getAllMovies();
     }
 
     @GetMapping("/search")
-    public List<MovieDTO> searchMovies(
-            @RequestParam("query") String searchQuery) {
+    List<Movie> searchMovies(
+            @RequestParam("query") final String searchQuery) {
         return movieService.searchMovies(searchQuery);
     }
 }
