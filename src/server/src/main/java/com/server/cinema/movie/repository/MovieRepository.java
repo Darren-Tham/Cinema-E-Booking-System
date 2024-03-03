33f7e61a1ff88919f64b40bb8b3acc407154954a
Darren Tham <darrent9859@gmail.com>
2024-03-03 01:30:59 -0500

    Update interface methods

--- src/server/src/main/java/com/server/cinema/movie/repository/MovieRepository.java
+++ src/server/src/main/java/com/server/cinema/movie/repository/MovieRepository.java
@@ -3,10 +3,9 @@ package com.server.cinema.movie.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.server.cinema.movie.entity.Movie;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
-    List<Movie> findByNameLike(String name);
+    List<Movie> findByMovieNameLike(final String movieName);
 }
