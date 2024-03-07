5d314efd6a1ed4036bc282df608ad61b80ea8d81
Darren Tham <darrent9859@gmail.com>
2024-03-07 18:04:53 -0500

    Remove HashMap import

--- server/src/main/java/com/server/cinema/util/DataCollectors.java
+++ server/src/main/java/com/server/cinema/util/DataCollectors.java
@@ -3,7 +3,6 @@ package com.server.cinema.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
