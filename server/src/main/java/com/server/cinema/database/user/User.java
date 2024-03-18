d181193f7ab3838bbf463c7df56826665b114701
Darren Tham <darrent9859@gmail.com>
2024-03-17 22:13:37 -0400

    Remove @Data import from User

--- server/src/main/java/com/server/cinema/database/user/User.java
+++ server/src/main/java/com/server/cinema/database/user/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Data;
 
 @MappedSuperclass
 public class User {
