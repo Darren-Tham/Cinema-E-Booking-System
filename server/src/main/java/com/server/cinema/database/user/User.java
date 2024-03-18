aef8470794500ae1e6e5a16be3f6b99d000441fd
Darren Tham <darrent9859@gmail.com>
2024-03-17 22:12:03 -0400

    Add abstract User class

---
+++ server/src/main/java/com/server/cinema/database/user/User.java
@@ -0,0 +1,24 @@
+package com.server.cinema.database.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@MappedSuperclass
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(insertable = false, updatable = false)
+    private int id;
+
+    @Column(nullable = false)
+    private String password;
+
+    protected User() {
+    }
+
+}
