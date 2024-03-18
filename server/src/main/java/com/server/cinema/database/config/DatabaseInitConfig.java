312bf385a2a57fbfdc813a6456aa0aaee8ad492a
Darren Tham <darrent9859@gmail.com>
2024-03-18 10:44:38 -0400

    Update DatabaseInitConfig and remove ShowTImeDataManager

--- server/src/main/java/com/server/cinema/database/config/DatabaseInitConfig.java
+++ server/src/main/java/com/server/cinema/database/config/DatabaseInitConfig.java
@@ -22,7 +22,6 @@ class DatabaseInitConfig {
             @Qualifier("directorDataManager") final InitRunnable directorRunnable,
             @Qualifier("castMemberDataManager") final InitRunnable castMemberRunnable,
             @Qualifier("reviewDataManager") final InitRunnable reviewRunnable,
-            @Qualifier("showTimeDataManager") final InitRunnable showTimeRunnable,
             @Qualifier("movieProducerDataManager") final InitRunnable movieProducerRunnable,
             @Qualifier("movieDirectorDataManager") final InitRunnable movieDirectorRunnable,
             @Qualifier("movieCastMemberDataManager") final InitRunnable movieCastMemberRunnable) {
@@ -32,8 +31,9 @@ class DatabaseInitConfig {
                 directorRunnable,
                 castMemberRunnable,
                 reviewRunnable,
-                showTimeRunnable,
-                movieProducerRunnable, movieDirectorRunnable, movieCastMemberRunnable);
+                movieProducerRunnable,
+                movieDirectorRunnable,
+                movieCastMemberRunnable);
     }
 
     @Bean
