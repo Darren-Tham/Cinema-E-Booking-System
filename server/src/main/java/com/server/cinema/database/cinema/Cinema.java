c642e3ffc471719f2eb22e286c77f6d3dc35772a
Darren Tham <darrent9859@gmail.com>
2024-03-18 10:46:57 -0400

    Add Cinema class

---
+++ server/src/main/java/com/server/cinema/database/cinema/Cinema.java
@@ -0,0 +1,35 @@
+package com.server.cinema.database.cinema;
+
+import java.util.Set;
+
+import com.server.cinema.database.show_room.ShowRoom;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Cinema {
+
+    @Id
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(columnDefinition = "DATE", nullable = false)
+    private String openingHours;
+
+    @Column(columnDefinition = "DATE", nullable = false)
+    private String closingHours;
+
+    @OneToMany(mappedBy = "cinema")
+    private Set<ShowRoom> showRooms;
+
+}
--- server/src/main/java/com/server/cinema/database/show_room/ShowRoom.java
+++ server/src/main/java/com/server/cinema/database/show_room/ShowRoom.java
@@ -2,6 +2,7 @@ package com.server.cinema.database.show_room;
 
 import java.util.Set;
 
+import com.server.cinema.database.cinema.Cinema;
 import com.server.cinema.database.movie.Movie;
 import com.server.cinema.database.show_time.ShowTime;
 
@@ -10,7 +11,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -33,5 +36,9 @@ public class ShowRoom {
 
     @ManyToMany(mappedBy = "showRooms")
     private Set<Movie> movies;
+
+    @ManyToOne
+    @JoinColumn(name = "cinema_name", nullable = false)
+    private Cinema cinema;
 
 }
