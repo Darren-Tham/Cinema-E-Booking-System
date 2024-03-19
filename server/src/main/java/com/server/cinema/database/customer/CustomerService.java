85c6d57c012eaa00a5cd60d482a908926e9e720e
Darren Tham <darrent9859@gmail.com>
2024-03-19 19:35:25 -0400

    Update CustomerService to convert DTO to Customer class

--- server/src/main/java/com/server/cinema/database/customer/CustomerService.java
+++ server/src/main/java/com/server/cinema/database/customer/CustomerService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.server.cinema.database.customer.dao.CustomerDAO;
+import com.server.cinema.database.customer.enums.UserState;
 
 @Service
 public class CustomerService {
@@ -16,10 +17,17 @@ public class CustomerService {
         this.customerDAO = customerDAO;
     }
 
-    public int addInactiveCustomer(final InactiveCustomerDTO customer) {
-        final String encryptedPassword = BCrypt.hashpw(customer.getPassword(), BCrypt.gensalt());
-        customer.setPassword(encryptedPassword);
-        return customerDAO.addInactiveCustomer(customer.toCustomer());
+    public int addInactiveCustomer(final InactiveCustomerDTO customerDTO) {
+        final String encryptedPassword = BCrypt.hashpw(customerDTO.password(), BCrypt.gensalt());
+        Customer customer = new Customer(
+                customerDTO.firstName(),
+                customerDTO.lastName(),
+                customerDTO.email(),
+                encryptedPassword,
+                customerDTO.phoneNumber(),
+                UserState.INACTIVE,
+                customerDTO.isSubscribedForPromotions());
+        return customerDAO.addInactiveCustomer(customer);
     }
 
 }
--- server/src/main/java/com/server/cinema/database/customer/InactiveCustomerDTO.java
+++ server/src/main/java/com/server/cinema/database/customer/InactiveCustomerDTO.java
@@ -1,31 +1,10 @@
 package com.server.cinema.database.customer;
 
-import com.server.cinema.database.customer.enums.UserState;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class InactiveCustomerDTO {
-
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
-    private String phoneNumber;
-    private boolean isSubscribedForPromotions;
-
-    public Customer toCustomer() {
-        return new Customer(
-                firstName,
-                lastName,
-                email,
-                password,
-                phoneNumber,
-                UserState.INACTIVE,
-                isSubscribedForPromotions);
-    }
+public record InactiveCustomerDTO(
+        String firstName,
+        String lastName,
+        String email,
+        String password,
+        String phoneNumber,
+        boolean isSubscribedForPromotions) {
 }
