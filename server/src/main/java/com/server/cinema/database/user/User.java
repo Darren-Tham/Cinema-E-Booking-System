5f7baf6a785e25ac186dc8c845268b8fcab6f251
Darren Tham <darrent9859@gmail.com>
2024-03-19 09:14:48 -0400

    Add @Data to User

--- server/src/main/java/com/server/cinema/database/user/User.java
+++ server/src/main/java/com/server/cinema/database/user/User.java
@@ -5,7 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
 
+@Data
 @MappedSuperclass
 public class User {
 
