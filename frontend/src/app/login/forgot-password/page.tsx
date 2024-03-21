901bea0285665fa021f9a3cc3c44eeceb872def6
Darren Tham <darrent9859@gmail.com>
2024-03-20 20:19:09 -0400

    Fetch to see if email exists in database

--- frontend/src/app/login/forgot-password/page.tsx
+++ frontend/src/app/login/forgot-password/page.tsx
@@ -3,9 +3,12 @@
 import HomeNavbar from "@/components/HomeNavbar"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
+import { useRef } from "react"
 
 export default function ForgotPassword() {
   const router = useRouter()
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
   return (
     <div className="min-h-screen bg-black flex flex-col">
       <HomeNavbar />
@@ -13,18 +16,31 @@ export default function ForgotPassword() {
         <div className="bg-dark-jade rounded-sm p-8 w-96 flex flex-col gap-3 items-center">
           <h1 className="h1">Reset Password</h1>
           <p className="text-white text-center font-semibold">
-            Enter the email address or phone number associated with your
-            account.
+            Enter the email address associated with your account.
           </p>
           <div className="flex flex-col w-full gap-1">
             <label htmlFor="username" className="label">
-              Email Address Or Phone Number
+              Email Address
             </label>
-            <input type="text" id="username" className="input" />
+            <input type="text" id="username" className="input" ref={inputRef} />
           </div>
           <button
             className="action-button w-full"
-            onClick={() => router.push("./reset-password")}
+            onClick={async () => {
+              const email = inputRef.current?.value
+              const response = await fetch(
+                `http://localhost:8080/api/customer/id/${email}`
+              )
+              if (response.ok) {
+                const data = await response.text()
+                const customerId = parseInt(data)
+                router.push("./reset-password")
+              } else {
+                alert(
+                  "The email inputted is not associated with any account in our system. Please try again or create a new account."
+                )
+              }
+            }}
           >
             Send Reset Code
           </button>
