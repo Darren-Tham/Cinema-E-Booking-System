081d29dc16ad4a0c9b088992a77ec36f99521a3e
Khoa Le <kl58277@uga.edu>
2024-03-25 10:04:45 -0400

    fixed cookies bugs

--- frontend/src/lib/Auth.ts
+++ frontend/src/lib/Auth.ts
@@ -7,23 +7,24 @@ const secretKey = "swe4050"
 const key = new TextEncoder().encode(secretKey)
 
 export async function decrypt(input: string): Promise<any> {
+
     const {payload} = await jwtVerify(input, key, {
         algorithms: ["HS256"],
-    })
+    }) 
     return payload
+    
 }
 
 export async function encrypt (payload: any) {
-    return await new SignJWT(payload).setProtectedHeader({alg: "HS256"}).setIssuedAt().setExpirationTime("15 sec from now").sign(key)
+    return await new SignJWT(payload).setProtectedHeader({alg: "HS256"}).setIssuedAt().setExpirationTime("1 hour from now").sign(key)
 }
 
 export async function initialSetUp(data: any) {
-    //const user = {id: data.customerId, email : data.email}
     let user = data
     user = {"email" : user[0], "user_id" : user[1]}
-    const expiration = new Date(Date.now() + 18 * 100000) // 30 minutes cookies
+    const expiration = new Date(Date.now() + (60 * 60 * 1000)) // 1 hour cookie
     const session = await encrypt({user, expiration})
-    cookies().set("session", session, {expires: expiration, httpOnly: true})
+    cookies().set("session", session, {expires: expiration, httpOnly: true, sameSite : "lax"})
 }
 
 export async function hasCookie(){
@@ -31,7 +32,7 @@ export async function hasCookie(){
     return !!val
 }
 export async function destroyCookie() {
-    cookies().set("session", "", {expires: new Date(0)})
+    cookies().set("session", "", {expires: new Date(0), sameSite : "lax"})
 }
 
 export async function getUser() {
@@ -45,13 +46,14 @@ export async function updateSession(request: NextRequest) {
     const session = request.cookies.get("session")?.value
     if (session) {
         const sessionData = await decrypt(session)
-        sessionData.expires = new Date(Date.now() + 15 * 1000)
+        sessionData.expires = new Date(Date.now() + (60 * 60 * 1000))  // 1 hour cookie
         const res = NextResponse.next()
         res.cookies.set({
             name: "session",
             value: await encrypt(sessionData),
             httpOnly: true,
-            expires: sessionData.expires
+            expires: sessionData.expires,
+            sameSite : "lax"
         })
         return res
     }
