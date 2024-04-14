d0f160484b5be78f035e31516d2a5aab6b503f99
Khoa Le <kl58277@uga.edu>
2024-04-14 19:42:45 -0400

    finish authentication for user and admin

--- frontend/src/app/order-confirmation/page.tsx
+++ frontend/src/app/order-confirmation/page.tsx
@@ -1,6 +1,8 @@
+"use client"
 import Link from "next/link"
-
+import { useAuth } from "@/lib/useAuth"
 export default function OrderConfirmation() {
+  const isUser = useAuth("user")
   const h1Styles = "font-bold text-5xl text-white text-center"
   const h1Styles2 = "text-1xl text-white text-center font-semibold"
   const h1Styles3 = "font-bold text-5xl text-white text-center underline"
@@ -8,31 +10,37 @@ export default function OrderConfirmation() {
     "text-white w-max font-bold px-4 py-2 rounded-md hover:scale-105 transition-transform duration-300 mt-2 bg-light-jade  min-w-[200px] min-h-[50px] underline"
 
   return (
-    <div className="h-screen bg-black">
-      <div className="bg-dark-jade flex flex-col gap-4 items-center p-12">
-        <h1 className={h1Styles}>Thank you for your order!</h1>
-      </div>
+    isUser ? (
+      <div className="h-screen bg-black">
+        <div className="bg-dark-jade flex flex-col gap-4 items-center p-12">
+          <h1 className={h1Styles}>Thank you for your order!</h1>
+        </div>
 
-      <div className="flex justify-center items-center">
-        <div className="bg-dark-jade pt-12 px-12 pb-10 rounded-lg flex flex-col gap-4 items-center mt-2">
-          <h1 className={h1Styles3}>Order Details</h1>
-          <h1 className={h1Styles2}>Order Confirmation Number: 62P-452QR8</h1>
-          <div>
-            <p className={h1Styles2}>Date: Thursday, Febuary 22, 2024</p>
-            <p className={h1Styles2}>Location: University 16 Cinema</p>
-            <p className={h1Styles2}>Time 12:00pm</p>
+        <div className="flex justify-center items-center">
+          <div className="bg-dark-jade pt-12 px-12 pb-10 rounded-lg flex flex-col gap-4 items-center mt-2">
+            <h1 className={h1Styles3}>Order Details</h1>
+            <h1 className={h1Styles2}>Order Confirmation Number: 62P-452QR8</h1>
+            <div>
+              <p className={h1Styles2}>Date: Thursday, Febuary 22, 2024</p>
+              <p className={h1Styles2}>Location: University 16 Cinema</p>
+              <p className={h1Styles2}>Time 12:00pm</p>
+            </div>
+            <div>
+              <p className={h1Styles2}>Adult Ticket x 2</p>
+              <p className={h1Styles2}>Child Ticket x 1</p>
+              <p className={h1Styles2}>Promotion Discount: $0.00</p>
+              <p className={h1Styles2}>Total: $37.99</p>
+            </div>
+            <Link href="/" className="flex justify-center mt-4">
+              <button className={buttonStyles}>Continue to Home</button>
+            </Link>
           </div>
-          <div>
-            <p className={h1Styles2}>Adult Ticket x 2</p>
-            <p className={h1Styles2}>Child Ticket x 1</p>
-            <p className={h1Styles2}>Promotion Discount: $0.00</p>
-            <p className={h1Styles2}>Total: $37.99</p>
-          </div>
-          <Link href="/" className="flex justify-center mt-4">
-          <button className={buttonStyles}>Continue to Home</button>
-          </Link>
         </div>
       </div>
-    </div>
+    ) : (
+      <div className="h-screen bg-black flex justify-center items-center">
+        <h1 className="text-white text-3xl">WOMP WOMP, you are not authorized.</h1>
+      </div>
+    )
   )
 }
--- frontend/src/app/seats/page.tsx
+++ frontend/src/app/seats/page.tsx
@@ -1,73 +1,81 @@
+"use client"
 import Image from "next/image"
 import HomeNavbar from "@/components/HomeNavbar"
 import Seat from "@/components/Seat"
 import CheckoutBanner from "@/components/CheckoutBanner"
 import Link from "next/link"
-
+import { useAuth } from "@/lib/useAuth"
 export default function SeatsPage() {
+  const isUser = useAuth("user")
   return (
-    <div className="min-h-screen bg-black flex flex-col">
-      <HomeNavbar />
-      <CheckoutBanner />
-      <div className="px-20 py-10 flex flex-col items-center gap-10">
-        <div className="bg-neutral-500 h-96 w-full" />
-        <div className="grid grid-cols-8 grid-rows-4 w-max gap-3">
-          <Seat />
-          <Seat />
-          <div />
-          <div />
-          <div />
-          <div />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat unavailable />
-          <Seat unavailable />
-          <Seat unavailable />
-          <Seat />
-          <Seat />
-          <Seat />
-          <div />
-          <div />
-          <Seat />
-          <Seat />
-          <Seat />
-          <Seat />
-          <div />
-          <div />
-        </div>
-        <div className="flex justify-between w-full">
-          <Link
-            href="/ticket-summary"
-            className="border-[3px] text-white font-semibold w-max py-3 px-20 text-xl rounded-md scale-transition"
-          >
-            Back
-          </Link>
-          <Link
-            href="/checkout-info"
-            className="bg-jade text-white font-semibold w-max py-3 px-20 text-xl rounded-md scale-transition"
-          >
-            Next
-          </Link>
+    isUser ? (
+      <div className="min-h-screen bg-black flex flex-col">
+        <HomeNavbar />
+        <CheckoutBanner />
+        <div className="px-20 py-10 flex flex-col items-center gap-10">
+          <div className="bg-neutral-500 h-96 w-full" />
+          <div className="grid grid-cols-8 grid-rows-4 w-max gap-3">
+            <Seat />
+            <Seat />
+            <div />
+            <div />
+            <div />
+            <div />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat unavailable />
+            <Seat unavailable />
+            <Seat unavailable />
+            <Seat />
+            <Seat />
+            <Seat />
+            <div />
+            <div />
+            <Seat />
+            <Seat />
+            <Seat />
+            <Seat />
+            <div />
+            <div />
+          </div>
+          <div className="flex justify-between w-full">
+            <Link
+              href="/ticket-summary"
+              className="border-[3px] text-white font-semibold w-max py-3 px-20 text-xl rounded-md scale-transition"
+            >
+              Back
+            </Link>
+            <Link
+              href="/checkout-info"
+              className="bg-jade text-white font-semibold w-max py-3 px-20 text-xl rounded-md scale-transition"
+            >
+              Next
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    ) : (
+      <div className="h-screen bg-black flex justify-center items-center">
+        <h1 className="text-white text-3xl">WOMP WOMP, you are not authorized.</h1>
+      </div>
+    )
   )
 }
