c3262f969fea3c73562f7a8a065f3d6195d69032
Khoa Le <kl58277@uga.edu>
2024-04-29 13:28:56 -0400

    changed movie banner ui

--- frontend/src/components/MainMovieBanner.tsx
+++ frontend/src/components/MainMovieBanner.tsx
@@ -17,34 +17,30 @@ export default function MainMovieBanner() {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentMovie((curr) => (curr + 1) % movies.length)
-    }, 2000)
+    }, 3000)
     return () => clearInterval(interval)
   }, [movies])
   const width = 275
   const height = 200
   return (
-    <div className="flex flex-row gap-4 justify-center bg-dark-jade h-96" >
-      <div
-          className="transform transition-transform duration-1000"
-          style={{ transform: `translateX(${(0 - currentMovie) * (width + 4)}px)` }}>
-      <Image src={movies[currentMovie]?.["imageLink"]} alt="Movie" width={width} height={height} />
+    <div className="flex flex-col justify-center mt-4">
+      <div className="flex flex-row justify-center h-96">
+      {movies.map((movie, index) => (
+          <Image
+            key={index}
+            src={movie?.["imageLink"]}
+            alt="Movie"
+            width={width}
+            height={height}
+            className={`absolute transition-opacity duration-1000 ${
+              index === currentMovie ? "opacity-100" : "opacity-0"
+            } shadow-[0_35px_60px_-15px_rgba(255,2555,255,0.3)]`}
+          />
+        ))}
       </div>
-      <div
-          className="transform transition-transform duration-1000"
-          style={{ transform: `translateX(${( 1 -currentMovie ) * (width + 4)}px)` }}>
-        <Image src={movies[(currentMovie + 1 ) % movies.length]?.["imageLink"]} alt="Movie" width={width} height={height} />
-      </div>
-      <div
-          className="transform transition-transform duration-1000"
-          style={{ transform: `translateX(${(2 - currentMovie ) * (width + 4)}px)` }}>
-        <Image src={movies[(currentMovie + 2 ) % movies.length]?.["imageLink"]} alt="Movie" width={width} height={height} />
-      </div>
-      <div
-          className="transform transition-transform duration-1000"
-          style={{ transform: `translateX(${(3- currentMovie ) * (width + 4)}px)` }}>
-        <Image src={movies[(currentMovie + 3 ) % movies.length]?.["imageLink"]} alt="Movie" width={width} height={height} />
-      </div>
-
+      <h1 className={`mt-10 text-white text-center text-xl`}>{movies[currentMovie]?.["title"]}</h1>
     </div>
+
+
   )
 }
