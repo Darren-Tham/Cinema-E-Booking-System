f97b216145964fa95b53f230ab8851817a5d9bf8
Darren Tham <darrent9859@gmail.com>
2024-04-30 15:27:49 -0400

    Remove imports

--- server/src/main/java/com/server/cinema/database/customer/CustomerController.java
+++ server/src/main/java/com/server/cinema/database/customer/CustomerController.java
@@ -3,8 +3,6 @@ package com.server.cinema.database.customer;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
