30dcd523b2dfea01b4111345684e65d1f1455569
Darren Tham <darrent9859@gmail.com>
2024-05-02 13:04:55 -0400

    Temp

--- server/src/main/java/com/server/cinema/config/sh.java
+++ server/src/main/java/com/server/cinema/config/sh.java
@@ -15,13 +15,13 @@ import com.server.cinema.config.interfaces.InitRunnable;
 import com.server.cinema.config.interfaces.MapSupplier;
 
 @Component
-public class ShowtimeDataManager extends DataManager implements InitRunnable {
+public class sh extends DataManager implements InitRunnable {
 
     private final MapSupplier<Movie> movieMapSupplier;
     private final ShowtimeRepository showTimeRepository;
 
     @Autowired
-    public ShowtimeDataManager(
+    public sh(
             final ResourceLoader resourceLoader,
             final MapSupplier<Movie> movieMapSupplier,
             final ShowtimeRepository showTimeRepository) {
