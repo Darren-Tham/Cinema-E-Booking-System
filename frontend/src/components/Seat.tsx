6e16e2183eb2ccc6b6eb95dae34d4ba20183f115
Khoa Le <kl58277@uga.edu>
2024-05-02 18:11:48 -0400

    get seats to cookies

--- frontend/src/components/Seat.tsx
+++ frontend/src/components/Seat.tsx
@@ -8,16 +8,18 @@ import { useState } from "react"
 
 type Props = {
   unavailable?: boolean
-  seatKey?: String | Number
 }
 
-export default function Seat({ unavailable, seatKey }: Readonly<Props>) {
+export default function Seat({ unavailable }: Readonly<Props>) {
   const [isSelected, setIsSelected] = useState(false)
   const iconWidth = 75
+
   return unavailable ? (
     <Image src={RedChairIcon} alt="Unavaiable Seat" width={iconWidth} />
   ) : (
-    <button onClick={() => setIsSelected(!isSelected)}>
+    <button onClick={() => {
+      setIsSelected(!isSelected)
+    }}>
       <Image
         src={isSelected ? GreenChairIcon : WhiteChairIcon}
         alt="Available Seat"
