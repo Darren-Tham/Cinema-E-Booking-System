3886b8f3de0a13f9af16185617f667efeac5b03e
Darren Tham <darrent9859@gmail.com>
2024-05-02 10:09:44 -0400

    Format

--- server/src/main/java/com/server/cinema/dto/CustomerDTO.java
+++ server/src/main/java/com/server/cinema/dto/CustomerDTO.java
@@ -2,6 +2,12 @@ package com.server.cinema.dto;
 
 import com.server.cinema.enums.UserState;
 
-public record CustomerDTO(int id, String firstName, String lastName, String email, String phoneNumber,
-        UserState status, boolean isSubscribedForPromotions) {
+public record CustomerDTO(
+        int id,
+        String firstName,
+        String lastName,
+        String email,
+        String phoneNumber,
+        UserState status,
+        boolean isSubscribedForPromotions) {
 }
